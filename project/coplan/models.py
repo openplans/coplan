34c7f2fe115ae29e5274e778741d2e1fcdab4934
Mjumbe and/or Paul <mjumbewu@gmail.com>
2012-06-19 22:27:14 +0000

    Add models for coplan Plan, Link, Image, and Video

--- project/coplan/models.py
+++ project/coplan/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Plan (models.Model):
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+    
+    title = models.CharField(max_length=100)
+    motivation = models.CharField(max_length=140)
+    details = models.TextField()
+    owner = models.ForeignKey('auth.User')
+    
+    location_lat = models.FloatField()
+    location_lon = models.FloatField()
+
+    supporters = models.ManyToManyField('auth.User')
+
+
+class Link (models.Model):
+    plan = models.ForeignKey('coplan.Plan', related_name='links')
+    url = models.URLField()
+
+
+class Image (models.Model):
+    plan = models.ForeignKey('coplan.Plan', related_name='images')
+    data = models.ImageField()
+
+
+class Video (models.Model):
+    plan = models.ForeignKey('coplan.Plan', related_name='videos')
+    url = models.URLField()
