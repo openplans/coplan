445520724cf8a8ec6e0bbbfc10829d9d1845055c
Mjumbe and/or Paul <mjumbewu@gmail.com>
2012-06-22 15:44:20 +0000

    Add djangorestframework urls for upgraded version of app (0.4.0-dev).

--- project/coplan/urls.py
+++ project/coplan/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from . import api
 from . import views
 
@@ -9,6 +9,8 @@ urlpatterns = patterns(
     url(r'^plans/$', views.new_plan, name='new_plan'),
     url(r'^plans/(?P<pk>\d+)$', views.edit_plan, name='edit_plan'),
 
+    url(r'^api/', 
+        include('djangorestframework.urls', namespace='djangorestframework')),
     url('^api/v1/plans/$', 
         api.PlanListView.as_view(), name='plan_list'),
     url('^api/v1/plans/(?P<pk>\d+)', 
