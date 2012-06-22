ebe43e17b58ca72a3e2c9c7440a80d39840661b8
Mjumbe and/or Paul <mjumbewu@gmail.com>
2012-06-22 05:56:24 +0000

    Instead of listing all fields explicitly, just set the exclude on PlanResource to an empty list

--- project/coplan/resources.py
+++ project/coplan/resources.py
@@ -3,14 +3,9 @@ from . import models
 
 class PlanResource (resources.ModelResource):
     model = models.Plan
-    fields = ['owner', 
-              'created_datetime', 
-              'updated_datetime', 
-              'title', 
-              'motivation', 
-              'details',
-              'id',
-              'url',]
+
+    # By default, id is excluded.  Override exclude to be empty.
+    exclude = []
 
     def owner(self, plan):
         return plan.owner.pk
